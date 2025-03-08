9dd084c42d2e3a0cfda1bdbd599f46c8fd2d9932
Hoki <wahhoki07@gmail.com>
2025-03-09 02:16:17 +0800

    Update satuantemperatur.py

--- satuantemperatur.py
+++ satuantemperatur.py
@@ -1,7 +1,19 @@
-echo "# physicsforpython" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/WahHoki/physicsforpython.git
-git push -u origin main
+# latihan konversi satuan temperatur
+# program konversi celcius ke satuan lain
+print("\nProgram konversi temperatur\n")
+
+# Celcius
+celcius = float(input("Masukan suhu dalam celcius : "))
+print("suhu adalah : ", celcius,"Celcius")
+
+# reamur
+reamur = (4/5) * celcius
+print ("suhu dalam reamur adalah : ",reamur, "Reamur")
+
+# fahrenheit
+fahrenheit = (9/5) * celcius
+print ("suhu dalam fahrenheit adalah : ",fahrenheit, "Fahrenheit")
+
+# kelvin
+kelvin = celcius + 273
+print ("suhu dalam kelvin adalah : ", kelvin, "Kelvin")
