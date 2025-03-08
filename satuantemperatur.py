ba805f52d4aee28b2e626adabedc2cec28bb1359
Hoki <wahhoki07@gmail.com>
2025-03-09 02:10:07 +0800

    Create satuantemperatur.py

---
+++ satuantemperatur.py
@@ -0,0 +1,7 @@
+echo "# physicsforpython" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/WahHoki/physicsforpython.git
+git push -u origin main
